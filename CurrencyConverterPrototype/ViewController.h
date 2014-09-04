2238d2c9554646437dd2e40bb3380120207b4838
Jeffrey Linwood <jlinwood@gmail.com>
2014-09-04 14:48:35 -0500

    Initial Commit

---
+++ CurrencyConverterPrototype/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CurrencyConverterPrototype
+//
+//  Created by Jeffrey Linwood on 9/4/14.
+//  Copyright (c) 2014 Jeff Linwood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
